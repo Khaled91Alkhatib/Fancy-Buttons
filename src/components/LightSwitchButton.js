bdeb8d63ce51b79ad0f2bd9c45db5c5784d5c3fc
Khaled Alkhatib <khaledkhatib91@hotmail.com>
2022-05-12 14:24:46 -0400

    Remove state and apply props

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,19 +1,19 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function LightSwitchButton() {
-  const [light, setLight] = useState("off");
+function LightSwitchButton(props) {
+
   const handleClick = () => {
-    if(light === "on") {
-      setLight("off")
+    if(props.light === "on") {
+      props.setLight("off")
     } else {
-      setLight("on")
+      props.setLight("on")
     }
   }
 
   return (
   <button className="LightSwitchButton"onClick={handleClick}>
-    {light === "on" && <span><i>💡</i> I'm on!</span>}
-    {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    {props.light === "on" && <span><i>💡</i> I'm on!</span>}
+    {props.light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
   </button>
   );
 }
